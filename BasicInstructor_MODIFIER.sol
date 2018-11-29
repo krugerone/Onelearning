311c9ea6b846b515cfdae46dbfeba033ddff0617
Renan Kruger <renan.kruger@superdigital.com.br>
2018-11-29 08:59:02 -0200

    WEB3

--- BasicInstructor_MODIFIER.sol
+++ BasicInstructor_MODIFIER.sol
@@ -1,17 +1,35 @@
 pragma solidity >=0.4.22 <0.6.0;
 
-contract BasicContract { 
-string fName; 
-uint age; 
+contract BasicContract {
+    
+   string fName;
+   uint age;
+   address owner;
+   
+    event Instructor(
+       string name,
+       uint age
+    );
 
-    function setInstructor(string _fName, uint _age) public { 
-    fName = _fName; 
-    age = _age; 
-    } 
-
-    function getInstructor() public constant returns (string, uint) { 
-    return (fName, age); 
-    } 
+   constructor() public {     
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+    
+   function setInstructor(string _fName, uint _age) onlyOwner public {
+       fName = _fName;
+       age = _age;
+       emit Instructor(_fName, _age);
+   }
+   
+   function getInstructor() public constant returns (string, uint) {
+       return (fName, age);
+   }
+    
 }
 
    
