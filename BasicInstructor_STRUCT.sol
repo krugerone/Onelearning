e2d6d55b4b3f0438185d780cfd4f08b35f1f9633
Renan Kruger <renan.kruger@superdigital.com.br>
2018-11-29 08:53:21 -0200

    WEB3

--- BasicInstructor_STRUCT.sol
+++ BasicInstructor_STRUCT.sol
@@ -12,12 +12,7 @@ contract StructContract {
     address[] public instructorAccts;
     
     function setInstructor(address _address, uint _age, string _fName, string _lName) public {
-        var instructor = instructors[_address];
-        
-        instructor.age = _age;
-        instructor.fName = _fName;
-        instructor.lName = _lName;
-        
+        instructors[_address] = Instructor(_age, _fName, _lName); 
         instructorAccts.push(_address) -1;
     }
     
